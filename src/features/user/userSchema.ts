0e1053f3f02c8938ff216ec03c58eb07d374d616
Alchernix <alchernix149@naver.com>
2025-11-07 00:46:50 +0900

    feat(auth): 로그인 폼 기본 동작 및 회원가입 스키마 추가

---
+++ src/features/user/userSchema.ts
@@ -0,0 +1,20 @@
+// 회원가입, 닉네임 변경 등 유저 정보를 form으로 받아야 할 때
+// 검증하기 위한 스키마
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  nickname: z
+    .string()
+    .min(2, { message: "닉네임은 2글자 이상이어야 해요." })
+    .max(10, { message: "닉네임은 10자 이하여야 해요." })
+    .regex(/^[가-힣a-zA-Z]+$/, {
+      message: "닉네임은 한글/영문만 사용할 수 있어요.",
+    }),
+  email: z.string().email({ message: "올바른 이메일 형식이 아니에요." }),
+  password: z
+    .string()
+    .min(8, { message: "비밀번호는 8자 이상이어야 해요." })
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/, {
+      message: "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 해요.",
+    }),
+});
--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,19 +1,63 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { useState } from "react";
+import { UserSchema } from "@/features/user/userSchema";
+import { useDebouncedCallback } from "use-debounce";
 
 export const Route = createFileRoute("/(auth)/login")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 입력 후 일정 시간이 지나고 검증
+  const validateEmail = useDebouncedCallback((value: string) => {
+    const EmailSchema = UserSchema.pick({ email: true });
+    const result = EmailSchema.safeParse({ email: value });
+    if (!result.success) {
+      setEmailError(result.error.issues[0].message);
+    } else {
+      setEmailError("");
+    }
+  }, 500);
+
   return (
     <div className="flex flex-col gap-5">
       <Link to="/">뒤로가기</Link>
       <h1>이메일로 로그인하기</h1>
-      <label htmlFor="">이메일</label>
-      <input type="email" name="" id="" />
-      <label htmlFor="">비밀번호</label>
-      <input type="password" name="" id="" />
-      <button className={``}>로그인</button>
+      <label htmlFor="email">이메일</label>
+      <input
+        onChange={(e) => {
+          setEmail(e.target.value);
+          setEmailError(""); // 입력 중 에러 문구 X
+          validateEmail(e.target.value);
+        }}
+        value={email}
+        type="email"
+        name="email"
+        id="email"
+      />
+      {emailError && <p>{emailError}</p>}
+      <label htmlFor="password">비밀번호</label>
+      <input
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+        type="password"
+        name="password"
+        id="password"
+      />
+      {/* 이메일 오류 없고, 비밀번호 1자 이상 작성한 경우 활성화 */}
+      <button
+        className={`${
+          !emailError && !!password ? "bg-blue-200" : "bg-slate-200"
+        }`}
+      >
+        로그인
+      </button>
     </div>
   );
 }

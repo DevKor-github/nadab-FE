f9dc22a860b2412d6b6f3e651c066caeab546481
Alchernix <alchernix149@naver.com>
2025-11-06 13:29:36 +0900

    chore: ESLint 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import reactHooks from "eslint-plugin-react-hooks";
+import react from "eslint-plugin-react";
+
+export default defineConfig([
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginRouter.configs["flat/recommended"],
+  reactHooks.configs.flat.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  {
+    settings: {
+      react: { version: "detect" },
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/only-throw-error": [
+        "error",
+        {
+          allow: [
+            {
+              from: "package",
+              package: "@tanstack/router-core",
+              name: "Redirect",
+            },
+          ],
+        },
+      ],
+    },
+  },
+  { ignores: ["temp.js", "config/*", "eslint.config.mjs"] },
+]);

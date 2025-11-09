a3078352be836b20f2594705230ac5061ddc9fa8
Alchernix <alchernix149@naver.com>
2025-11-09 22:58:48 +0900

    test: 모바일 가로 여백 테스트

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,7 @@ function RootComponent() {
   return (
     <>
       <div className="min-h-dvh w-vw flex">
-        <div className="w-dvw sm:w-[412px] sm:mx-auto px-[6.25vw] sm:px-4 bg-white overflow-x-hidden">
+        <div className="w-dvw sm:w-[412px] sm: sm:mx-auto px-padding-x-m sm:px-4 sm:pb-4 bg-white overflow-x-hidden">
           <Outlet />
         </div>
       </div>

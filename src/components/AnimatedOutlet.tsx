404a91036ea6eeefed2b6038281b52d9799381e1
Alchernix <alchernix149@naver.com>
2025-11-20 23:01:19 +0900

    test: freeze 원인 찾는 중

--- src/components/AnimatedOutlet.tsx
+++ src/components/AnimatedOutlet.tsx
@@ -20,7 +20,7 @@ export const AnimatedOutlet = forwardRef<HTMLDivElement>((_, ref) => {
       ref={ref}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      // exit={{ opacity: 0 }}
       transition={{ duration: 0.3, ease: "easeOut" }}
       className="h-full"
     >
